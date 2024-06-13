c6e9859c721cce14c2083b1617d8a3b1632c0980
ohanz <chiaguguadaniel@gmail.com>
2024-06-14 00:26:04 +0100

    firstcommit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello It's Ohanz o!</Text>
+      <Text> </Text>
+      <Text>We Are Here</Text>
+    </View>
+  );
+}
