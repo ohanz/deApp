f1d470cef1cabcc6c33df9d855e06ee4cf82e941
ohanz <chiaguguadaniel@gmail.com>
2024-06-14 03:30:57 +0100

    restructured and fixed on android

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView } from "react-native";
 import { Dimensions } from 'react-native';
 
 var screenWidth = Dimensions.get('window').width; //full screen width
 
 const myStyle = StyleSheet.create({
+  dMain: {
+   flex: 1, 
+  },
+  box: {
+    // backgroundColor:'white',
+    backgroundColor: 'rgba(255,255,255,0.6)',
+    height: 45,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 20,
+    elevation: 20,
+  },
   containerMain: {
     flex: 1,
     alignItems: 'center',
+  },
+  mScroll:{
+   paddingBottom: 10, paddingStart: 5, 
   },
   footerStyle: {
     position: 'absolute',
     bottom: 0,
     width: screenWidth,
-    height: 50
-    , backgroundColor: "#009900",
+    height: 50,
+     backgroundColor: "#009900",
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -25,7 +41,6 @@ const showYear = date.getFullYear();
 
 export default function Index() {
   return (
-    
     // <View
     //   style={{
     //     flex: 1,
@@ -46,13 +61,17 @@ export default function Index() {
     //   </View>
     // </View>
 
-    <View style={{flex: 1, backgroundColor:'white'}}>
+
+    <View
+      style={myStyle.dMain}>
       {/* header */}
+      <View style={myStyle.box}>
       <Text style={{fontSize: 25, textAlign: "center"}}>Hello Hyper, Ohanz!</Text>
+      </View>
+      
       {/* Body */}
-    {/* <View style={{flex: 1, backgroundColor: 'red'}} /> */}
-    <View style={{flex: 0.9, backgroundColor: '#eee'}}>
-        <ScrollView>
+      <View style={{flex: 0.9, backgroundColor: '#eee'}}>
+      <ScrollView style={myStyle.mScroll}>
             <Text style={{marginBottom: 50}}>scrollable section</Text>
             <Text> Main Content Here</Text>
           <Text> Main Content Here</Text>
@@ -74,18 +93,31 @@ export default function Index() {
           <Text> Main Content Here</Text>
           <Text> Main Content Here</Text>
           <Text> Main Content Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
+          <Text> HyperContent Here</Text>
         </ScrollView>
-    </View>
+        </View>
 
-    <View style={myStyle.footerStyle}>
+        <View style={myStyle.footerStyle}>
       {/* footer */}
       <Text style={{color: "#fff", fontSize: 20,
       }}>Copyright&copy;{showYear}</Text>
-      
-    </View> 
+      </View>
+
+    </View>
 
 
-</View>
+    
     
   );
 }
